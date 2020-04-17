917a82396f75c65440b6f72c87856fa896c9531e
AllisonChacon1207 <62119402+AllisonChacon1207@users.noreply.github.com>
2020-04-16 23:59:05 -0600

    Add files via upload

---
+++ TAREA 9 ALLISON CHACON/ArrayList.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuario;
+
+/**
+ *
+ * @author Allison
+ */
+class ArrayList<T> {
+
+    void add(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ TAREA 9 ALLISON CHACON/Persona.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuario;
+
+/**
+ *
+ * @author Allison
+ */
+public class Persona {
+    String nombre,apellido,cuenta;
+
+    public Persona(String nombre, String apellido, String cuenta) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cuenta = cuenta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+    
+}
